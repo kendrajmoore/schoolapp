2265ef7fa1fe5037e41815205d98837c39c29cbe
Kendra Moore <kendra.j.moore3443@gmail.com>
2017-10-10 11:34:58 -0700

    Tuesday

--- controllers/schools.js
+++ controllers/schools.js
@@ -2,41 +2,42 @@ var School = require('../models/school.js');
 
 module.exports = function(app) {
 
-// INDEX
-app.get('/', function (req, res) {
-  School.find(function(err, schools) {
-    res.render('schools-index', {schools: schools});
+  // INDEX
+  app.get('/', function (req, res) {
+    School.find(function(err, schools) {
+      res.render('schools-index', {schools: schools});
+    });
   });
-});
 
-
-// NEW
-app.get('/schools/new', function (req, res) {
-  res.render('schools-new', {});
-})
-
-app.post('/schools', function (req, res) {
-  School.create(req.body, function(err, school) {
-    res.redirect('/schools/' + school._id);
+  // NEW
+  app.get('/schools/new', function (req, res) {
+    res.render('schools-new', {});
   })
-})
 
-app.get('/schools/:id', function (req, res) {
-  School.findById(req.params.id).exec(function (err, school) {
-    res.render('schools-show', {school: school});
+  // CREATE
+  app.post('/schools', function (req, res) {
+    School.create(req.body, function(err, school) {
+      res.redirect('/schools/' + school._id);
+    })
   })
-})
 
-//Update
+  // SHOW
+  app.get('/schools/:id', function (req, res) {
+    School.findById(req.params.id).exec(function (err, school) {
+      res.render('schools-show', {school: school});
+    })
+  })
 
-app.put('/schools/:id', function (req, res) {
+  // UPDATE
+  app.put('/schools/:id', function (req, res) {
     console.log(req.body)
-  School.findByIdAndUpdate(req.params.id,  req.body, function(err, school) {
-    res.redirect('/schools/' + school._id);
-  })
-})
 
-app.get('/schools/:id/edit', function (req, res) {
+    School.findByIdAndUpdate(req.params.id,  req.body, function(err, school) {
+      res.redirect('/schools/' + school._id);
+    })
+  })
+
+  app.get('/schools/:id/edit', function (req, res) {
     School.findById(req.params.id, function(err, school) {
       res.render('schools-edit', {school: school});
     })
--- scripts/get-schools.js
+++ scripts/get-schools.js
@@ -1,18 +1,31 @@
-var mongoose = require('mongoose'),
 var request = require('request');
-request('https://data.sfgov.org/resource/mmsr-vumy.json', function (error, response, body) {
-  // console.log('error:', error); // Print the error if one occurred
-  // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  // console.log('body:', body); // Print the HTML for the Google homepage.
-  // console.log(JSON.parse(body));
-//
-//   // connect to db
-//   // use your model
-//   // save new schools with attributes from JSON
-//
 
+// connect to db
+var mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/schoolapp');
+
+// use your model
+var School = require('../models/school.js');
+
+
+request('https://data.sfgov.org/resource/mmsr-vumy.json', function (error, response, body) {
+  // GET THE SCHOOLS FROM API
   var body = JSON.parse(body);
+  // console.log(body)
   for (i = 0; i < body.length; i++) {
-    console.log(body[i].name)
+    // SAVE NEW SCHOOL
+    var school = {
+      title: body[i].campus_name,
+      address: body[i].campus_address,
+      grades: body[i].grade_range
+    }
+    // console.log(school)
+    School.create(school, function(err, school) {
+      console.log(err);
+      console.log(school);
+    });
+
+    // console.log(body)
   }
+
 });
